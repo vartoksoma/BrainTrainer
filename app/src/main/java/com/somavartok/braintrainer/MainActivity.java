821c3d0ff93ea92c1fde5f25d7fc1da043f2a346
Vartok Soma <vartok.soma@gmail.com>
2019-03-06 10:59:00 +0100

    setup app, basic buttons and some color

---
+++ app/src/main/java/com/somavartok/braintrainer/MainActivity.java
@@ -0,0 +1,30 @@
+package com.somavartok.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void chooseAnswer(View view){
+
+
+
+    }
+
+    public void start(View view){
+
+        startButton.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.Gobutton);
+    }
+}
